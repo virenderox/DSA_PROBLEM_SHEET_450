cd241348c82b53254dab418725e8836b64060046
virenderox <43112861+virenderox@users.noreply.github.com>
2022-05-16 11:55:44 +0530

    Create main.py

---
+++ Array/PROB - 2/MAX and MIN/main.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+def getMinMax(a, n):
+    max_ = float('-inf')
+    min_ = float('inf')
+    
+    for i in a:
+        if i > max_:
+            max_ = i
+            
+        if i < min_:
+            min_ = i
+            
+    return min_,max_
+            
+                
+    
+    
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = getMinMax(a, n)
+        print(product[0], end=" ")
+        print(product[1])
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends
