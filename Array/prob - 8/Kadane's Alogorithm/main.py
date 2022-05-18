9a1a546a9f92bd10f664e40b2d719352a9356e9b
virenderox <43112861+virenderox@users.noreply.github.com>
2022-05-18 08:02:52 +0530

    Create main.py

---
+++ Array/prob - 8/Kadane's Alogorithm/main.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        max_sum = float('-inf')
+        end = 0
+        
+        for val in arr:
+            end = end + val
+            
+            if max_sum < end :
+                max_sum = end
+                
+            if end < 0:
+                end = 0
+                
+        return max_sum
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
